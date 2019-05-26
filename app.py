a551c7ecc732aaed731df9492665185e7a7986e3
Daniel Butum <danibutum@gmail.com>
2019-05-26 15:53:51 +0300

    update heroku

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from common import *
 import cli
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    return app
+
+app = create_app()
 
 @app.route("/")
 def home():
@@ -11,7 +19,10 @@ def home():
     referendum_data = process_json_data(download_json_data(PAGE_REFERENDUM))
     difference_data = euro_data - referendum_data
 
-    return render_template("index.html", euro_data=euro_data, difference_data=difference_data, referendum_data=referendum_data, url_euro=PAGE_EURO, url_referendum=PAGE_REFERENDUM)
+    return render_template("index.html", refresh_seconds=60,\
+        euro_data=euro_data, \
+        difference_data=difference_data, referendum_data=referendum_data, \
+        url_euro=PAGE_EURO, url_referendum=PAGE_REFERENDUM)
 
 if __name__ == "__main__":
     app.run(debug=True)
